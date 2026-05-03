4b2265b214629ee8d2ab7b309600da44ffdf49e9
nikakoroleova <nikakoroleova@gmail.com>
2026-05-03 11:57:52 +0200

    feat: implement search

---
+++ src/components/PokemonSearch/Search/Search.tsx
@@ -0,0 +1,44 @@
+import React, { type ReactNode } from 'react';
+import './Search.css';
+
+type SearchProps = {
+  onSearch: (textInput: string) => void;
+};
+
+type SearchState = {
+  searchInputValue: string;
+};
+
+export default class Search extends React.Component<SearchProps, SearchState> {
+  constructor(props: SearchProps) {
+    super(props);
+
+    this.state = {
+      searchInputValue: localStorage.getItem('search_input_value') || '',
+    };
+  }
+
+  changeInputValue = (e: React.ChangeEvent<HTMLInputElement>) =>
+    this.setState({ searchInputValue: e.target.value });
+
+  searchData = () => {
+    localStorage.setItem('search_input_value', this.state.searchInputValue);
+    this.props.onSearch(this.state.searchInputValue);
+  };
+
+  render(): ReactNode {
+    return (
+      <section className="search-section">
+        <input
+          type="text"
+          value={this.state.searchInputValue}
+          onChange={this.changeInputValue}
+          placeholder="Enter the name of the Pokémon..."
+        />
+        <button className="pokemon-button" onClick={this.searchData}>
+          Search
+        </button>
+      </section>
+    );
+  }
+}
